5cb0d908bb09eb90240d63ad59568ff060281b64
ryan <ryanjustin@live.com>
2014-11-08 23:57:28 -0800

    added timer to splash

--- pythonproject/splash.py
+++ pythonproject/splash.py
@@ -12,8 +12,9 @@ top = Tk()
 
 top.geometry("700x800")
 B1 = Button(top, text = "simple calculator",height = 10,width = 12, command = lambda : subprocess.call("python simpleCalc.py", shell = True))
-
+B2 = Button(top, text = "simple timer",height = 10,width = 12, command = lambda : subprocess.call("python SimpleTimer.py", shell = True))
 B1.grid(column = 0, row = 0, padx = 10, pady =10)
+B2.grid(column = 1, row = 0, padx = 10, pady =10)
 
 
 
@@ -21,5 +22,3 @@ B1.grid(column = 0, row = 0, padx = 10, pady =10)
 
 
 top.mainloop()
-
-
